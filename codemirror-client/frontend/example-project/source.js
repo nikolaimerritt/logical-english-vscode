f361d2ecc7ca7f9be1d34cb0fe73c065003b8327
Nikolai Merritt <nikolaimerritt20@gmail.com>
2022-07-21 14:10:37 +0100

    Delete codemirror-client directory

--- codemirror-client/frontend/example-project/source.js
+++
@@ -1,6 +0,0 @@
-the templates are:
-*a person* likes *an object*
-  
-the knowledge base source includes:
-bob likes cats
-sarah likes dogs
--- codemirror-client/lib/server-capability-registration.d.ts
+++
@@ -1,4 +0,0 @@
-import { Registration, ServerCapabilities, Unregistration } from 'vscode-languageserver-protocol';
-declare function registerServerCapability(serverCapabilities: ServerCapabilities, registration: Registration): ServerCapabilities;
-declare function unregisterServerCapability(serverCapabilities: ServerCapabilities, unregistration: Unregistration): ServerCapabilities;
-export { registerServerCapability, unregisterServerCapability, };
